dd58800952797cb00a544a11252c3f832b0e0602
Ashikur Rahman <ashikbd08@gmail.com>
2023-11-10 10:40:04 +0600

    modified branding

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -2,8 +2,8 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ url('/') }}" class="brand-link">
-      <img src="{{ asset('resources/img/logo.jpg') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Eve Salon</span>
+      <!--<img src="{{ asset('resources/img/logo.jpg') }}" alt="CR-Inventory Logo" class="brand-image img-circle elevation-3" style="opacity: .8">-->
+      <span class="brand-text font-weight-light">CR-Inventory</span>
     </a>
 
     <!-- Sidebar -->
